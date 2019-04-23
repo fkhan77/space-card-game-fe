2a9f59f5c7d8a146001ebf603273e04b13b79f15
Farhad Khan <37068936+fkhan1996@users.noreply.github.com>
2019-04-23 14:27:52 +0100

    Extract out a startPage component from App.js
    styling has also been transferred and new file structure added for pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from "react";
-import createCard from "./Components/createCard";
-import shipLogo from "./Assets/singleShip.png";
-import gameTitle from "./Assets/gameTitle.png";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import startPage from "./Pages/startPage";
+import gamePage from "./Pages/gamePage";
+import endGamePage from "./Pages/endGamePage";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { playerTurn: 1, gameStage: "start" };
-  }
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={gameTitle} />
-          <div className='ship-logo-container'>
-            <img src={shipLogo} className='ship-logo' alt='ship-logo' />
+      // Might change this to use conditional rendering using state instead of a router
+      <Router>
+        <div>
+          <div className='App'>
+            <Route path='/' exact component={startPage} />
+            <Route path='/gamePage/' component={gamePage} />
+            <Route path='/endGamePage/' component={endGamePage} />
           </div>
-        </header>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
