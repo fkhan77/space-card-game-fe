5799d8e9120d6002f3264ead0d80e241aebf83e9
Farhad Khan <37068936+fkhan1996@users.noreply.github.com>
2019-04-23 14:28:55 +0100

    Add initial end game page

---
+++ src/Pages/endGamePage/endGamePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./endGamePage.css";
+
+class endGamePage extends Component {
+  render() {
+    return (
+      <div className='end-game-page-intro-text'>This is the game page</div>
+    );
+  }
+}
+
+export default endGamePage;
